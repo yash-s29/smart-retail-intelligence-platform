5689143b16a783b96414b452fb44033a0521eb95
Yash Patil <yp1664003@gmail.com>
2026-08-22 00:30:01 +0530

    Update Inventory.jsx

--- frontend/src/pages/inventory/Inventory.jsx
+++ frontend/src/pages/inventory/Inventory.jsx
@@ -48,7 +48,7 @@ import {
   actionButtonSx,
   primaryButtonSx,
   iconBadgeSx,
-} from "../../components/Inventory/inventoryTheme";
+} from "../../Inventory/inventoryTheme";
 
 
 // ==========================================================
